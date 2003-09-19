852db69fa3d116ee5d5356c1ac76435c86026411
Lori Corbani <Lori.Corbani@jax.org>
2003-09-19 18:09:02 +0000

    TR 5154

---
+++ J80501rnainsitu.py
@@ -0,0 +1,331 @@
+#!/usr/local/bin/python
+
+# $Header$
+# $Name$
+
+#
+# Program: J80501rnainsitu.py
+#
+# Original Author: Lori Corbani
+#
+# Purpose:
+#
+#	To translate RNA InSitu 10 and 14 input files into input files
+#	for the insituload.py program.
+#
+# Requirements Satisfied by This Program:
+#
+# Usage:
+#
+#	J80501rnainsitu.py
+#
+# Envvars:
+#
+# Inputs:
+#
+#       9.5_dpc_in_situ_results.txt, a tab-delimited file in the format:
+#               field 1: Paper ID
+#		field 2: Clone ID
+#		field 3: MGI clone ID
+#		field 4: MGI gene symbol
+#		field 5: MGI gene ID
+#		field 6: overall expression
+#		field 7-24: expression
+#		field 25-27: image file names
+#
+#	9.5dpc_in_situ_tissue.txt, a tab-delimited file in the format:
+#		field 1: Reported Tissue
+#		field 2: MGI Tissue
+#		field 3: Theiler Stage
+#
+# Outputs:
+#
+#       4 tab-delimited files:
+#
+#	In_Situ_probeprep.txt
+#	In_Situ_assay.txt
+#	In_Situ_specimen.txt
+#	In_Situ_results.txt
+#	
+#       Error file
+#
+# Exit Codes:
+#
+# Assumes:
+#
+# Bugs:
+#
+# Implementation:
+#
+
+import sys
+import os
+import string
+
+#globals
+
+TAB = '\t'		# tab
+CRT = '\n'		# carriage return/newline
+NULL = ''
+
+inInSituFile = ''	# file descriptor
+inTissueFile = ''	# file descriptor
+
+prepFile = ''		# file descriptor
+assayFile = ''          # file descriptor
+specimenFile = ''       # file descriptor
+resultsFile = ''        # file descriptor
+
+datadir = os.environ['INSITUDATADIR']
+
+inInSituFileName = datadir + '/tr5154/9.5_dpc_in_situ_results.txt'
+inTissueFileName = datadir + '/tr5154/9.5dpc_in_situ_tissue.txt'
+
+prepFileName = datadir + '/In_Situ_probeprep.txt'
+assayFileName = datadir + '/In_Situ_assay.txt'
+specimenFileName = datadir + '/In_Situ_specimen.txt'
+resultsFileName = datadir + '/In_Situ_results.txt'
+
+# constants for probe prep
+prepType = 'RNA'
+hybridization = 'Antisense'
+labelledWith = 'Digoxigenin'
+labelCoverage = 'continuously'
+visualizedWith = 'Alkaline phosphatase'
+
+# constants for Absent
+ABSENT = 'Absent'
+NA = 'Not Applicable'
+
+# constants for assay
+reference = 'J:80501'
+assayType = 'RNA In Situ'
+createdBy = os.environ['CREATEDBY']
+
+# constants for specimen
+specimenLabel = '%s 9.5dpc'
+genotype = 'MGI:2166653'	# NMRI
+age = 'embryonic day 9.5'
+ageNote = 'Staging was performed using morphological criteria.'
+sex = 'Not Specified'
+fixation = '4% Paraformaldehyde'
+embedding = 'Not Applicable'
+specimenHybridization = 'whole mount'
+specimenNote = NULL
+
+# translation of input file strengths and MGI strengths
+strengthTrans = {'(+)':'Present', '(+)rr':'Present', NULL:'Absent', 'ND':'Absent'}
+# translation of input file patterns and MGI patterns
+patternTrans = {'(+)':'Not Specified', '(+)rr':'Regionally Restricted', NULL:'Not Specified', 'ND':'Not Specified'}
+resultNoteTrans = {'Wh_emb':'The expression was widespread.'}
+
+# next available assay key
+assayKey = 1
+
+# Purpose: prints error message and exits
+# Returns: nothing
+# Assumes: nothing
+# Effects: exits with exit status
+# Throws: nothing
+
+def exit(
+    status,          # numeric exit status (integer)
+    message = None   # exit message (string)
+    ):
+
+    if message is not None:
+        sys.stderr.write('\n' + str(message) + '\n')
+ 
+    sys.exit(status)
+ 
+# Purpose: initialize
+# Returns: nothing
+# Assumes: nothing
+# Effects: initializes global variables
+#          exits if files cannot be opened
+# Throws: nothing
+
+def init():
+    global inInSituFile, inTissueFile
+    global prepFile, assayFile, specimenFile, resultsFile
+ 
+    try:
+        inInSituFile = open(inInSituFileName, 'r')
+    except:
+        exit(1, 'Could not open file %s\n' % inInSituFileName)
+
+    try:
+        inTissueFile = open(inTissueFileName, 'r')
+    except:
+        exit(1, 'Could not open file %s\n' % inTissueFileName)
+
+    try:
+        prepFile = open(prepFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % prepFileName)
+
+    try:
+        assayFile = open(assayFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % assayFileName)
+
+    try:
+        specimenFile = open(specimenFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % specimenFileName)
+
+    try:
+        resultsFile = open(resultsFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % resultsFileName)
+
+    return
+
+# Purpose:  processes data
+# Returns:  nothing
+# Assumes:  nothing
+# Effects:  writes data to output files
+# Throws:   nothing
+
+def process():
+
+    global assayKey
+
+    tissueTrans = {}	# maps input tissue to MGI tissue and Theiler Stage
+
+    lineNum = 0
+    for line in inTissueFile.readlines():
+	lineNum = lineNum + 1
+
+	tokens = string.split(line[:-1], TAB)
+	badTissue = tokens[0]
+	goodTissue = tokens[1]
+	theilerStage = tokens[2]
+
+	if lineNum == 1:
+	    continue
+
+	key = badTissue
+	value = goodTissue + '|' + theilerStage
+	tissueTrans[key] = value
+
+    # For each line in the input file
+
+    lineNum = 0
+    for line in inInSituFile.readlines():
+
+	lineNum = lineNum + 1
+
+        # Split the line into tokens
+        tokens = string.split(line[:-1], TAB)
+
+	# processing first line (header)
+	# grab the Tissue headings
+
+	if lineNum == 1:
+	    tissueLabels = tokens[5:23]
+	    continue
+
+	# else process an actual data line
+
+        try:
+	    paperID = tokens[0]
+	    cloneID = tokens[1]
+	    mgiCloneID = tokens[2]
+	    mouseGene = tokens[3]
+	    accID = tokens[4]
+	    results = tokens[5:23]
+	    imageFileName = tokens[24:26]
+
+        except:
+            print 'Invalid Line (%d): %s\n' % (lineNum, line)
+
+	if len(mouseGene) == 0:
+	    continue
+
+	# create one assay per record
+
+	# write the probe prep information
+
+	prepFile.write(str(assayKey) + TAB + \
+	    mgiCloneID + TAB + \
+	    prepType + TAB + \
+	    hybridization + TAB + \
+	    labelledWith + TAB + \
+	    labelCoverage + TAB + \
+	    visualizedWith + CRT)
+
+	# write the assay information
+
+	assayFile.write(str(assayKey) + TAB + \
+	    accID + TAB + \
+	    reference + TAB + \
+	    assayType + TAB + \
+	    TAB + \
+	    createdBy + CRT)
+
+        # write the specimen (one for each Assay)
+
+	specimenKey = 1
+
+	specimenFile.write(str(assayKey) + TAB + \
+	    str(specimenKey) + TAB + \
+	    specimenLabel % (mouseGene) + TAB + \
+	    genotype + TAB + \
+	    age + TAB + \
+	    ageNote + TAB + \
+	    sex + TAB + \
+	    fixation + TAB + \
+	    embedding + TAB + \
+	    specimenHybridization + TAB + \
+	    specimenNote + CRT)
+
+	# one result for each Tissue 
+
+	ignoreRemainingTissues = 0
+	resultKey = 1
+	for i in range(len(tissueLabels)):
+    
+	    # Translate the Tissue into a Tissue and Age
+	    [tissue, theilerStage] = string.split(tissueTrans[tissueLabels[i]], '|')
+
+	    if tissueLabels[i] == 'Wh_emb':
+	        strength = strengthTrans[results[i]]
+	        pattern = patternTrans[results[i]]
+	        resultNote = resultNoteTrans[tissueLabels[i]]
+	        if results[i] == '(+)':
+		    ignoreRemainingTissues = 1
+            else:
+	        strength = strengthTrans[results[i]]
+	        pattern = patternTrans[results[i]]
+		resultNote = NULL
+
+	    resultsFile.write(str(assayKey) + TAB + \
+	        str(specimenKey) + TAB + \
+	        str(resultKey) + TAB + \
+		strength + TAB + \
+		pattern + TAB + \
+		tissue + TAB + \
+		theilerStage + TAB + \
+		resultNote + CRT)
+
+	    resultKey = resultKey + 1
+
+	    if ignoreRemainingTissues:
+		break
+
+	specimenKey = specimenKey + 1
+	assayKey = assayKey + 1
+
+    # end of "for line in inInSituFile.readlines():"
+
+#
+# Main
+#
+
+init()
+process()
+exit(0)
+
+# $Log$
+#
---
+++ J80501rtpcr.py
@@ -0,0 +1,338 @@
+#!/usr/local/bin/python
+
+# $Header$
+# $Name$
+
+#
+# Program: J80501rtpcr.py
+#
+# Original Author: Lori Corbani
+#
+# Purpose:
+#
+#	To translate RT-PCR input files into input files
+#	for the assayload/gelload.py program.
+#
+# Requirements Satisfied by This Program:
+#
+# Usage:
+#
+#	J80501rtpcr.py
+#
+# Envvars:
+#
+# Inputs:
+#
+#       RT-PCR.txt, a tab-delimited file in the format:
+#		field 1: Human Gene Symbol
+#		field 2: Mouse Gene Symbol
+#               field 3: MGI Marker Accession ID
+#               field 4: Sequence 1
+#               field 5: Sequence 2
+#               field 6-7: Tissue expression
+#
+#	RT_PCR_tissue.txt, a tab-delimited file in the format:
+#		field 1: Reported Tissue
+#		field 2: MGI Tissue
+#		field 3: Theiler Stage
+#
+# Outputs:
+#
+#       5 tab-delimited files:
+#
+#	RT_PCR_primer.txt
+#	RT_PCR_probeprep.txt
+#	RT_PCR_assay.txt
+#	RT_PCR_gellane.txt
+#	RT_PCR_gelband.txt
+#
+#       Error file
+#
+# Exit Codes:
+#
+# Assumes:
+#
+# Bugs:
+#
+# Implementation:
+#
+
+import sys
+import os
+import string
+
+#globals
+
+TAB = '\t'		# tab
+CRT = '\n'		# carriage return/newline
+NULL = ''
+
+inRTPCRFile = ''	# file descriptor
+inTissueFile = ''	# file descriptor
+primerFile = ''         # file descriptor
+prepFile = ''		# file descriptor
+assayFile = ''          # file descriptor
+gellaneFile = ''        # file descriptor
+gelbandFile = ''        # file descriptor
+
+datadir = os.environ['RTPCRDATADIR']
+
+inRTPCRFileName = datadir + '/tr5154/RT-PCR.txt'
+inTissueFileName = datadir + '/tr5154/RT_PCR_tissue.txt'
+primerFileName = datadir + '/RT_PCR_primer.txt'
+prepFileName = datadir + '/RT_PCR_probeprep.txt'
+assayFileName = datadir + '/RT_PCR_assay.txt'
+gellaneFileName = datadir + '/RT_PCR_gellane.txt'
+gelbandFileName = datadir + '/RT_PCR_gelband.txt'
+
+# constants for primers
+regionCovered = NULL
+repeatUnit = NULL
+moreProduct = '0'
+productSize = NULL
+
+# constants for probe prep
+prepType = 'DNA'
+hybridization = 'Not Applicable'
+labelledWith = 'Not Applicable'
+labelCoverage = 'Not Applicable'
+visualizedWith = 'Not Specified'
+
+# constants for assay
+reference = 'J:80501'
+assayType = 'RT-PCR'
+createdBy = os.environ['CREATEDBY']
+
+# constants for gel lanes
+control = 'No'
+genotype = 'MGI:2166310'	# Not Specified
+sampleAmount = NULL
+rnaType = 'total'
+embryonicAgeNote = 'Age of embryo at noon of plug day not specified in reference.'
+sex = 'Not Specified'
+laneNote = NULL
+rowNote = NULL
+bandNote = NULL
+
+# constants for gel rows
+gelsize = NULL
+gelunits = 'Not Specified'
+
+mgiTypeKey = 8		# Assay
+mgiPrefix = "MGI:"
+
+# translation of ages to theiler stages
+
+ageTrans = {'28':'postnatal day 2', \
+    '23':'embryonic day 15.5'}
+
+# translation of input file band strengths and MGI band strengths
+
+bandTrans = {'+':'Present', '-':'Absent'}
+
+# Purpose: prints error message and exits
+# Returns: nothing
+# Assumes: nothing
+# Effects: exits with exit status
+# Throws: nothing
+
+def exit(
+    status,          # numeric exit status (integer)
+    message = None   # exit message (string)
+    ):
+
+    if message is not None:
+        sys.stderr.write('\n' + str(message) + '\n')
+ 
+    sys.exit(status)
+ 
+# Purpose: initialize
+# Returns: nothing
+# Assumes: nothing
+# Effects: initializes global variables
+#          exits if files cannot be opened
+# Throws: nothing
+
+def init():
+    global inRTPCRFile, inTissueFile, primerFile, prepFile, assayFile, gellaneFile, gelbandFile
+ 
+    try:
+        inRTPCRFile = open(inRTPCRFileName, 'r')
+    except:
+        exit(1, 'Could not open file %s\n' % inRTPCRFileName)
+
+    try:
+        inTissueFile = open(inTissueFileName, 'r')
+    except:
+        exit(1, 'Could not open file %s\n' % inTissueFileName)
+
+    try:
+        primerFile = open(primerFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % primerFileName)
+
+    try:
+        prepFile = open(prepFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % prepFileName)
+
+    try:
+        assayFile = open(assayFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % assayFileName)
+
+    try:
+        gellaneFile = open(gellaneFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % gellaneFileName)
+
+    try:
+        gelbandFile = open(gelbandFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % gelbandFileName)
+
+    return
+
+# Purpose:  processes data
+# Returns:  nothing
+# Assumes:  nothing
+# Effects:  writes data to output files
+# Throws:   nothing
+
+def process():
+
+    tissueTrans = {}	# maps input tissue to MGI tissue and Theiler Stage
+
+    for line in inTissueFile.readlines():
+	tokens = string.split(line[:-1], TAB)
+	badTissue = tokens[0]
+	goodTissue = tokens[1]
+	theilerStage = tokens[2]
+
+	key = badTissue
+	value = goodTissue + '|' + theilerStage
+	tissueTrans[key] = value
+
+#    assay = 0	# unique Assay ID, if heading exists
+    assay = 1	# unique Assay ID
+
+    # For each line in the input file
+
+    for line in inRTPCRFile.readlines():
+
+        # Split the line into tokens
+        tokens = string.split(line[:-1], TAB)
+
+	# processing first line (header)
+	# grab the Tissue headings
+
+	# hard-code headings for this file
+	tissueLabels = ['E15.5', 'P2']
+
+	if assay == 0:
+	    tissueLabels = tokens[5:-1]
+	    assay = assay + 1
+	    continue
+
+	# else process an actual data line
+
+        try:
+	    humanGene = tokens[0]
+	    mouseGene = tokens[1]
+	    accID = tokens[2]
+	    seq1 = tokens[3]
+	    seq2 = tokens[4]
+	    bands = tokens[5:]
+
+        except:
+            print 'Invalid Line (%d): %s\n' % (assay, line)
+
+	if len(mouseGene) == 0:
+	    continue
+
+	# write the primer information
+
+	primerFile.write(str(assay) + TAB + \
+	    accID + TAB + \
+	    mouseGene + '-pA,' + mouseGene + '-pB' + TAB + \
+	    reference + TAB + \
+	    regionCovered + TAB + \
+	    seq1 + TAB + \
+	    seq2 + TAB + \
+	    repeatUnit + TAB + \
+	    moreProduct + TAB + \
+	    productSize + CRT)
+
+	# write the probe prep information
+
+	prepFile.write(str(assay) + TAB + \
+	    prepType + TAB + \
+	    hybridization + TAB + \
+	    labelledWith + TAB + \
+	    labelCoverage + TAB + \
+	    visualizedWith + CRT)
+
+	# write the assay information
+
+	assayFile.write(str(assay) + TAB + \
+	    accID + TAB + \
+	    reference + TAB + \
+	    assayType + TAB + \
+	    createdBy + CRT)
+
+	# write the gel lane and gel band information
+
+	lane = 1
+	row = 1
+
+	# for each Tissue from the heading (Brain, Heart, etc.)
+
+	for i in range(len(tissueLabels)):
+
+	    # Translate the Tissue into a Tissue and Age
+	    [tissue, theilerStage] = string.split(tissueTrans[tissueLabels[i]], '|')
+
+	    if theilerStage == '28':
+		ageNote = NULL
+            else:
+		ageNote = embryonicAgeNote
+
+	    gellaneFile.write(str(assay) + TAB + \
+		str(lane) + TAB + \
+		tissueLabels[i] + TAB + \
+		genotype + TAB + \
+		rnaType + TAB + \
+		control + TAB + \
+		sampleAmount + TAB + \
+		sex + TAB + \
+		ageTrans[theilerStage] + TAB + \
+		ageNote + TAB + \
+		laneNote + TAB + \
+		tissue + TAB + \
+		theilerStage + CRT)
+
+	    gelbandFile.write(str(assay) + TAB + \
+	        str(lane) + TAB + \
+	        str(row) + TAB + \
+	        gelsize + TAB + \
+	        gelunits + TAB + \
+	        bandTrans[bands[i]] + TAB + \
+	        rowNote + TAB + \
+	        bandNote + CRT)
+
+	    lane = lane + 1
+
+	assay = assay + 1
+
+    #	end of "for line in inRTPCRFile.readlines():"
+
+#
+# Main
+#
+
+init()
+process()
+exit(0)
+
+# $Log$
+#
